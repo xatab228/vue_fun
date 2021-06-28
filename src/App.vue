3eb99aa86acfbb07fe68da231ed86fc311313d8d
xatab228 <xatab228@gmail.com>
2021-06-28 08:29:15 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@
         <v-chip
          class="ma-2" 
          close
-         @click="name_to_text_field(item.name,index)"
-         @click:close="visible(index)" 
+         @click="name_to_text_field(item.name,index,item._id)"
+         @click:close="visible(index,item._id)" 
          v-for="(item,index) in full_name" 
          v-bind:key="item._id"
          v-bind:index="index" 
@@ -49,23 +49,24 @@ export default {
   },
 
   data: () => ({
-    api_url : 'https://crudcrud.com/api/7cf97fbea55b40b4b38012ef25743517/name',
+    api_url : 'https://crudcrud.com/api/090d690756da4a3ba95575b89735b8c2/name',
     first_name : "Александр;Захар;Варвара;Кирилл;Тимофей;Вероника;Кира;Максим;Ярослав;Марк;Тихон;Макар;Матвей;Екатерина;Даниил;Ксения;Алексей;Артём;Юлия;Дарья;Мирон;Жасмин;Маргарита;Александра;Дмитрий;София;Агата;Софья;Егор;Злата",
     second_name : "Петровский;Фомин;Воробьева;Мальцев;Грачева;Степанов;Яковлев;Майоров;Власов;Соколов;Хомяков;Александрова;Кузьмина;Уткин;Плотников;Дмитриева;Григорьева;Лаврова;Некрасова;Митрофанов;Чернышев;Филиппова;Демидов;Назарова;Родионов;Смирнов;Пономарев;Орлов;Кузнецов;Чесноков",
     full_name: [],
-    chek: Array.from({length: 10}).map(item => item = true),
     name_for_change: null,
-    index_for_change: null
+    index_for_change: null,
+    single_api_index: null
   }),
   methods: {
     generated_names(first_name,second_name){
-      this.api_url += Math.floor(Math.random() * (100 - 0) + 0)
+      this.api_url += Math.floor(Math.random() * (1000 - 0) + 0)
       first_name = first_name.split(';');
       second_name = second_name.split(';');
       for (let i = 0;i<10;++i){
-        let full_name = first_name[Math.floor(Math.random() * (first_name.length - 0) + 0)] + " " + second_name[Math.floor(Math.random() * (first_name.length - 0) + 0)] 
+        let full_name = first_name[Math.floor(Math.random() * (first_name.length - 0) + 0)] + " " + second_name[Math.floor(Math.random() * (second_name.length - 0) + 0)] 
         axios.post(this.api_url,{name: full_name});
       }
+      console.log(this.api_url)
     },
     get_names(){
       axios
@@ -74,17 +75,19 @@ export default {
           this.full_name = response.data;
         });
     },
-    visible(index){
+    visible(index,index_g){
       document.getElementById(index).style.display = "none"
+      axios.delete(this.api_url + '/' + index_g)
     },
-    name_to_text_field(name,index){
+    name_to_text_field(name,index,index_g){
       this.name_for_change = name;
       this.index_for_change = index;
-      console.log(this.full_name[index].name)
+      this.single_api_index = index_g;
     },
     change_name(){
       let name = document.getElementById("text_edit").value
       this.full_name[this.index_for_change].name = name
+      axios.put(this.api_url + '/' + this.single_api_index,{name: name})
     }
   }
 };
